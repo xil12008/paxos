98977f664f43cb869c9273315f83421cf23b08d7
EC2 Default User <ec2-user@ip-172-31-45-151.us-west-2.compute.internal>
2015-11-01 22:27:51 +0000

    A

--- network.py
+++ network.py
@@ -1,24 +1,29 @@
 import threading
 import socket
 import time
+from configuration import Configuration
 
 def UDPClient():
-    print threading.currentThread().getName(), 'Starting'
-    UDP_IP = "127.0.0.1"
-    UDP_PORT = 5005
-    MESSAGE = "Hello, World!"
+    ID = Configuration.getMyID()
+    print threading.currentThread().getName(), 'UDP Client Starting. I am Node#', ID
+  
+    for ip in Configuration.IPTABLE:
+        UDP_IP = ip
+        UDP_PORT = Configuration.UDPPORT 
+        MESSAGE = "Hello, World! from node %d" % ID 
 
-    print "UDP target IP:", UDP_IP
-    print "UDP target port:", UDP_PORT
-    print "UDP sending message:", MESSAGE
-    sock = socket.socket(socket.AF_INET, # Internet
-                         socket.SOCK_DGRAM) # UDP
-    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
-    print threading.currentThread().getName(), 'Exiting'
+        print "UDP target IP:", UDP_IP
+        print "UDP target port:", UDP_PORT
+        print "UDP sending message:", MESSAGE
+        sock = socket.socket(socket.AF_INET, # Internet
+                             socket.SOCK_DGRAM) # UDP
+        sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+        print threading.currentThread().getName(), 'UDP Client Exiting. I am Node#', ID
     return
 
 def UDPServer():
-    print threading.currentThread().getName(), 'Starting'
+    ID = Configuration.getMyID()
+    print threading.currentThread().getName(), 'UDP Server Starting. I am Node#', ID
     UDP_IP = "127.0.0.1"
     UDP_PORT = 5005
 
@@ -29,28 +34,32 @@ def UDPServer():
     while True:
         data, addr = sock.recvfrom(20) # buffer size is 1024 bytes
         print "UDP received message:", data
-    print threading.currentThread().getName(), 'Exiting'
+    print threading.currentThread().getName(), 'UDP Server Exiting. I am Node#', ID
     return
 
 def TCPClient():
-    print threading.currentThread().getName(), 'Starting'
-    TCP_IP = '127.0.0.1'
-    TCP_PORT = 12345
-    BUFFER_SIZE = 1024
-    MESSAGE = "Hello, World!"
+    ID = Configuration.getMyID()
+    print threading.currentThread().getName(), 'TCP Client Starting. I am Node#', ID
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((TCP_IP, TCP_PORT))
-    s.send(MESSAGE)
-    data = s.recv(BUFFER_SIZE)
-    s.close()
-
-    print "TCP client received data:", data
-    print threading.currentThread().getName(), 'Exiting'
+    for ip in Configuration.IPTABLE:
+        TCP_IP = ip 
+        TCP_PORT = Configuration.TCPPORT 
+        BUFFER_SIZE = 1024
+        MESSAGE = "Hello, World! from Node#%d" % ID
+    
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((TCP_IP, TCP_PORT))
+        s.send(MESSAGE)
+        data = s.recv(BUFFER_SIZE)
+        s.close()
+    
+        print "NODE %d TCP client received data: %s" %( ID,  data)
+        print threading.currentThread().getName(), 'TCP Client Exiting. I am Node #', ID
     return
 
 def TCPServer():
-    print threading.currentThread().getName(), 'Starting'
+    ID = Configuration.getMyID()
+    print threading.currentThread().getName(), 'TCP Server Starting. I am Node#', ID
     TCP_IP = '127.0.0.1'
     TCP_PORT = 12345
     BUFFER_SIZE = 20  # Normally 1024, but we want fast response
@@ -60,14 +69,15 @@ def TCPServer():
     s.listen(1)
 
     conn, addr = s.accept()
-    print 'TCP Connection address:', addr
+    print 'TCP NODE#', ID,' Connection address:' , addr
+    print 'NODE#', ID,'<---connect----> NODE#' , Configuration.getID(addr)
     while 1:
         data = conn.recv(BUFFER_SIZE)
         if not data: break
-        print "TCP Server received data:", data
+        print "NODE %d TCP Server received data:%s" %( ID,  data)
         conn.send(data)  # echo
     conn.close()
-    print threading.currentThread().getName(), 'Exiting'
+    print threading.currentThread().getName(), 'TCP Server Exiting. I am NODE#', ID
     return
 
 tUDPServer = threading.Thread(target=UDPServer)
