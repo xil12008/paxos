f679c43a33e024dcfe67c56619094c7f9367feb6
EC2 Default User <ec2-user@ip-172-31-0-64.ec2.internal>
2015-11-12 21:18:27 +0000

    Acceptor

--- acceptor.py
+++ acceptor.py
@@ -1,11 +1,12 @@
 import threading
+import ast
 import pdb
 import json
 import sys
 import select
 import socket
 import time
-import pickle
+import csv
 from configuration import Configuration
 
 #tag:print
@@ -25,49 +26,111 @@ def acceptor_udp_server():
     while True:
         data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
         peerID = Configuration.getID( addr[0] )
-        printdata("Acceptor UDP Recv", ID, peerID, ID, data)
-        try:
-            pdb.set_trace()
-            json_object = json.loads(data.strip())
-            if json_object['msgname'] == "commit": 
-               onCommit(json_object['entryID'], json_object['msglist']) 
-        except:
-            print "Can't parse data:", data
+        #try:
+        json_object = json.loads(data.strip())
+        if json_object['msgname'] == "commit": 
+           onCommit(json_object['entryID'], json_object['msglist']) 
+           printdata("Acceptor Recv Commit", ID, peerID, ID, data)
+        elif json_object['msgname'] == "accept":
+           onAccept(json_object['entryID'], json_object['msglist']) 
+           printdata("Acceptor Recv Accept", ID, peerID, ID, data)
+        elif json_object['msgname'] == "prepare":
+           onPrepare(json_object['entryID'], json_object['msglist']) 
+           printdata("Acceptor Recv Prepare", ID, peerID, ID, data)
+        #except:
+        #    print "Can't parse data:", data, sys.exc_info()[0]
     print threading.currentThread().getName(), ' Acceptor UDP Server Exiting. I am Node#', ID
     return
 
-def onPrepare():
-    pass
+def firstRun():
+    locallog = readlog() 
+    if not locallog: 
+        for i in range( 0, 10): 
+            if i not in locallog:
+                locallog[i] = {'maxPrepare': Configuration.init_maxPrepare,\
+                               'accNum': Configuration.init_accNum,\
+                               'accVal': Configuration.init_accVal,\
+                               'value' : Configuration.init_value }
+    savelog(locallog)
 
-def onAccept():
-    pass
+def onPrepare(entryID, msglist):
+    print "onPrepare:", entryID, ",", msglist
+    m = int(msglist[0])
+    entryID = str(entryID)
+    locallog = readlog()
+    if ( m > int(locallog[entryID]["maxPrepare"]) ):
+        locallog[entryID]["maxPrepare"] = m
+        send_msglist = [locallog[entryID]["accNum"], locallog[entryID]["accVal"]]
+        print "send promise"
+        udp_send(entryID, "promise", send_msglist) 
+    else:
+        print "rejected"
+    savelog(locallog)
+
+def onAccept(entryID, msglist):
+    print "onAccept:", entryID, ",", msglist
+    m = int(msglist[0])
+    entryID = str(entryID)
+    v = msglist[1]
+    locallog = readlog()
+    if ( m >= int(locallog[entryID]['maxPrepare']) ):
+        locallog[entryID]['accNum'] = m
+        locallog[entryID]['accVal'] = v 
+        send_msglist = [locallog[entryID]['accNum'], locallog[entryID]['accVal']]
+        print "send ack"
+        udp_send(entryID, "ack", send_msglist) 
+    else:
+        print "rejected"
+    savelog(locallog)
 
 def onCommit(entryID, msglist):
-    locallog = pickle.load( open( "locallog.p", "rb" ) )
-    locallog[entryID] = msglist[0]
-    pickle.dump( favorite_color, open( "locallog.p", "wb" ) )
+    print "onCommit:", entryID, ",", msglist
+    locallog = readlog()
+    locallog[entryID]["value"] = msglist[0]
+    savelog(locallog)
 
-def udp_send(): 
+def udp_send(entryID, msgname, msglist): 
     ID = Configuration.getMyID()
     UDP_IP = Configuration.getPublicIP() 
     UDP_PORT = Configuration.ACCEPTOR_PORT
     msg = {}
-    msg['msgname'] = "commit"
-    msg['entryID'] = 10 
-    msg['msglist'] = ["test value"]
+    msg['msgname'] = msgname 
+    msg['entryID'] = str( entryID ) 
+    msg['msglist'] = msglist 
     MESSAGE = json.dumps(msg) 
-    printdata("UDP Send ", ID, ID, Configuration.getID( UDP_IP ) , MESSAGE )
+    #printdata("UDP Send" + msgname, ID, ID, Configuration.getID( UDP_IP ) , MESSAGE )
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
     sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
 
+def savelog(dict):
+    w = csv.writer(open("log.csv", "w+"))
+    for key, val in dict.items():
+        w.writerow([key, val])
+
+def readlog():
+    dict = {}
+    try:
+        for key, val in csv.reader(open("log.csv")):
+            dict[key] = ast.literal_eval(val)
+    finally:
+        return dict
+
+firstRun()
 
 tAcceptor = threading.Thread(target=acceptor_udp_server)
 tAcceptor.daemon = True
 tAcceptor.start()
 
-time.sleep(5)
+time.sleep(2)
 
-udp_send()
+#udp_send( 1, "commit", ["value hahaha"] ) 
+#udp_send( 1, "commit", ["value hahaha"] ) 
+udp_send( 3, "prepare", ["4"] ) 
+udp_send( 3, "prepare", ["2"] ) 
+udp_send( 3, "prepare", ["5"] ) 
+#udp_send( 3, "prepare", ["2"] ) 
+udp_send( 3, "accept", ["5", "my test value"] ) 
+udp_send( 3, "commit", ["my test value"] ) 
 
 while threading.active_count() > 0:
     time.sleep(0.1)
