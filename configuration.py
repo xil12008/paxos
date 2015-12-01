0325a8f4c59dbd89c611def471e07c6df35229c0
xniu7 <nx.niuxiang@gmail.com>
2015-11-30 19:08:09 -0500

    local

--- configuration.py
+++ configuration.py
@@ -6,9 +6,9 @@ from urllib2 import urlopen
 '''
 
 class Configuration:
-    IPTABLE = ['52.34.135.9','52.27.173.157']
+    #IPTABLE = ['52.34.135.9','52.27.173.157']
     #IPTABLE = ['52.1.87.58','52.18.239.201','54.94.201.162']
-    #IPTABLE = ['localhost']
+    IPTABLE = ['129.161.71.163']
     UDPPORT = 5005 #UDP PORT
     TCPPORT = 12345 #TCP PORT
     PORTS = {"prepare":12345, "promise":12346, "maxPrepare":12346, "accept":12345, "ack":12348, "commit":12345, "event":12350, "complete":12351}
