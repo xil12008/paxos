3c11ae0396d7c851b8fc6466510bf90c59ad6525
xil12008 <luxy622@gmail.com>
2015-12-03 14:29:04 -0500

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -6,7 +6,7 @@ from urllib2 import urlopen
 '''
 
 class Configuration:
-    IPTABLE = ['129.161.71.163', '129.161.71.244']
+    IPTABLE = ['128.213.11.178']
     #UDPPORT = 5005 #UDP PORT
     TCPPORT = 13333 #TCP PORT
     PORTS = {"prepare":12345, "promise":12346, "maxPrepare":12346, "accept":12345, "ack":12348, "commit":12345, "event":12350, "complete":12351}
